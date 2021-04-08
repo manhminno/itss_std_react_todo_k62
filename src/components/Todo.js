d9524d44cdbfe69873e394440de8242fa019550d
manhminno <manh6264@gmail.com>
2021-04-08 08:26:15 +0000

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -41,6 +41,15 @@ function Todo() {
     putItems([...items, {key: getKey(), text, done:false}]);
   };
   
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+  const handleFilterChange = value => setFilter(value);
+  
   return (
     <div className="panel">
       <div className="panel-heading">
@@ -48,6 +57,11 @@ function Todo() {
       </div>
       <Input onAdd={handleAdd} />
       {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem
           key={item.key}
           item={item}
@@ -55,7 +69,7 @@ function Todo() {
         />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
