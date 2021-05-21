456dbdb5ad44d827925d1ac11c020723ddf48aa5
manhminno <manh6264@gmail.com>
2021-05-21 07:27:35 +0000

    Save todo in firestore

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,77 +1,58 @@
 import React, { useState } from 'react';
-
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
-/* コンポーネント */
+import {getKey} from "../lib/util";
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
+import useFirestore from '../hooks/useFirestore';
 
-/* カスタムフック */
-import useStorage from '../hooks/storage';
-
-/* ライブラリ */
-import {getKey} from "../lib/util";
-
-function Todo() {
-  const [items, putItems, clearItems] = useStorage();
-
-  const handleCheck = checked => {
-    const newItems = items.map(item => {
-      if (item.key === checked.key) {
-        item.done = !item.done;
-      }
-      return item;
-    });
-    putItems(newItems);
+ function Todo() {
+    const [items, addItem, updateItem, clearItems] = useFirestore();
+    const [filter, setFilter] = React.useState('ALL');
+    const displayItems = items.filter(item => {
+     if (filter === 'ALL') return true;
+     if (filter === 'TODO') return !item.done;
+     if (filter === 'DONE') return item.done;
+   });
+   const handleCheck = checked => {
+    updateItem(checked);
+ };
+ const handleAdd = text => {
+     addItem({ text, done: false });
   };
   
-  const handleAdd = text => {
-    putItems([...items, {key: getKey(), text, done:false}]);
-  };
-  
-  const [filter, setFilter] = React.useState('ALL');
-
-  const displayItems = items.filter(item => {
-    if (filter === 'ALL') return true;
-    if (filter === 'TODO') return !item.done;
-    if (filter === 'DONE') return item.done;
-  });
   const handleFilterChange = value => setFilter(value);
-  
-  return (
-    <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
-      <Input onAdd={handleAdd} />
-      {items.map(item => (
-      <Filter
-        onChange={handleFilterChange}
-        value={filter}
-      />
-      {displayItems.map(item => (
-        <TodoItem
-          key={item.key}
-          item={item}
-          onCheck={handleCheck}
-        />
-      ))}
-      <div className="panel-block">
-        {displayItems.length} items
-      </div>
-      <div className="panel-block">
-        <button className="button is-light is-fullwidth" onClick={clearItems}>
-          全てのToDoを削除
-        </button>
-      </div>
-    </div>
-  );
-}
+   return (
+    <article class="panel is-danger">
+       <div className="panel-heading">
+       <span class="icon-text">
+           <span class="icon">
+             <i class="fas fa-calendar-check"></i>
+           </span>
+           <span> ITSS Todoアプリ</span>
+         </span>
+       </div>
+       <Input onAdd={handleAdd} />
+       <Filter
+         onChange={handleFilterChange}
+         value={filter}
+       />
+       {displayItems.map(item => (
+         <TodoItem 
+           key={item.id}
+           item={item}
+           onCheck={handleCheck}
+         />
+       ))}
+       <div className="panel-block">
+       {displayItems.length} items
+       </div>
+       <div className="panel-block">
+         <button className="button is-light is-fullwidth" onClick={clearItems}>
+           全てのToDoを削除
+         </button>
+       </div>
+       </article>
+   );
+ }
 
-export default Todo;
+export default Todo;  
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -9,4 +9,58 @@ var firebaseConfig = {
     appId: "1:1052191436339:web:caf631defb338510847d46"
 };
 
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyB9kPk3HIkyW7J99D4CdLtkwQkc74R2jqQ",
+    authDomain: "fir-sample-e4e31.firebaseapp.com",
+    projectId: "fir-sample-e4e31",
+    storageBucket: "fir-sample-e4e31.appspot.com",
+    messagingSenderId: "1052191436339",
+    appId: "1:1052191436339:web:caf631defb338510847d46"
+};
+
 firebase.initializeApp(firebaseConfig); 
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+}; 
