1584d8c226ea11872a2bf89471e6c7413c92f456
manhminno <manh6264@gmail.com>
2021-05-21 07:45:56 +0000

    Login by Authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,4 @@
 import firebase from 'firebase'
-
 var firebaseConfig = {
     apiKey: "AIzaSyB9kPk3HIkyW7J99D4CdLtkwQkc74R2jqQ",
     authDomain: "fir-sample-e4e31.firebaseapp.com",
@@ -8,21 +7,11 @@ var firebaseConfig = {
     messagingSenderId: "1052191436339",
     appId: "1:1052191436339:web:caf631defb338510847d46"
 };
-
-import firebase from 'firebase'
-var firebaseConfig = {
-    apiKey: "AIzaSyB9kPk3HIkyW7J99D4CdLtkwQkc74R2jqQ",
-    authDomain: "fir-sample-e4e31.firebaseapp.com",
-    projectId: "fir-sample-e4e31",
-    storageBucket: "fir-sample-e4e31.appspot.com",
-    messagingSenderId: "1052191436339",
-    appId: "1:1052191436339:web:caf631defb338510847d46"
-};
-
-firebase.initializeApp(firebaseConfig); 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
 
 export const getFirebaseItems = async () => {
   try {
@@ -38,7 +27,6 @@ export const getFirebaseItems = async () => {
     return [];
   }
 }
-
 export const addFirebaseItem = async (item) => {
   try {
     const todoRef = db.collection("todos");
@@ -47,7 +35,6 @@ export const addFirebaseItem = async (item) => {
     console.log(err);
   }
 }
-
 export const updateFirebaseItem = async (item, id) => {
   try {
     const todoRef = db.collection("todos").doc(id);
@@ -56,11 +43,36 @@ export const updateFirebaseItem = async (item, id) => {
     console.log(err);
   }
 }
-
 export const clearFirebaseItem = async (item) => {
   const todoRef = db.collection("todos").doc(item.id);
   await todoRef.delete().then(function () {
   }).catch(function (err) {
     console.log(err);
   });
-}; 
+  
+};
+
+export const uiConfig = {
+  signInFlow: 'popup',
+  signInSuccessUrl: "/",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+}
+
+export const storeUserInfo = async (user) => {
+  const { uid } = user;
+  const userDoc = await db.collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await db.collection("users").doc(uid).set({ name: user.displayName });
+    return {
+      name: user.displayName,
+      id: uid,
+    };
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    };
+  }
+}  
